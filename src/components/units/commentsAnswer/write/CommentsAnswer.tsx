054b4afc087200acd44ed83a7cac487fec7c56be
jiwon0995 <jiwon0995@gmail.com>
2021-11-14 13:03:10 +0900

    Add : 댓글페이지 -대댓글 등록,수정,삭제,조회 기능 구현

---
+++ src/components/units/commentsAnswer/write/CommentsAnswer.tsx
@@ -0,0 +1,152 @@
+import { useMutation } from "@apollo/client";
+import styled from "@emotion/styled";
+import { useState } from "react";
+import {
+  IMutation,
+  IMutationCreateUseditemQuestionAnswerArgs,
+  IMutationUpdateUseditemQuestionAnswerArgs,
+} from "../../../../commons/types/generated/types";
+import {
+  CREATE_USEDITEM_QUESTION_ANSWER,
+  FETCH_USEDITEM_QUESTION_ANSWERS,
+  UPDATE_USEDITEM_QUESTION_ANSWER,
+} from "./CommentsAnswer.queries";
+
+const CommnetsWriteInput = styled.textarea`
+  width: 358px;
+  height: 150px;
+  padding: 12px 12px 0px 12px;
+  border: #d3d3d3 1px solid;
+  font-family: "NotoSansKRregular";
+  margin-top: 10px;
+  resize: none;
+  display: block;
+  ::placeholder {
+    color: #d3d3d3;
+    position: absolute;
+    font-family: "NotoSansKRregular";
+  }
+`;
+const CommnetsInput = styled.input`
+  width: 200px;
+  height: 42px;
+  background-color: #f3f3f3;
+  border: none;
+  padding-left: 12px;
+`;
+const CommentsBnt = styled.div`
+  width: 79px;
+  height: 42px;
+  background-color: #8915a6;
+  font-family: "NotoSansKRregular";
+  font-size: 16px;
+  color: #fff;
+  text-align: center;
+  line-height: 42px;
+  cursor: pointer;
+`;
+
+export default function CommentsAnswer(props) {
+  console.log("Comments", props.Comments);
+
+  const [createUseditemQuestionAnswer] = useMutation<
+    Pick<IMutation, "createUseditemQuestionAnswer">,
+    IMutationCreateUseditemQuestionAnswerArgs
+  >(CREATE_USEDITEM_QUESTION_ANSWER);
+  
+  const [updateUseditemQuestionAnswer] = useMutation<
+    Pick<IMutation, "updateUseditemQuestionAnswer">,
+    IMutationUpdateUseditemQuestionAnswerArgs
+    >(UPDATE_USEDITEM_QUESTION_ANSWER);
+  
+  // 대댓글 contents state
+  const [contents, setContents] = useState("");
+  // 대댓글 취소 함수
+  const onClickIsOpen = () => {
+    props.IsOpen(false);
+  };
+
+  // 대댓글 입력 저장 함수
+  const onChangeAnswer = (e) => setContents(e.target.value);
+
+  const onClickIsEdit = (e) => props.setIsEdit(false);
+
+  // 대댓글 등록 함수
+  const onClickSubmitAnswer = async () => {
+    try {
+      const result = await createUseditemQuestionAnswer({
+        variables: {
+          createUseditemQuestionAnswerInput: { contents },
+          useditemQuestionId: String(props.Comments?._id),
+        },
+        refetchQueries: [
+          {
+            query: FETCH_USEDITEM_QUESTION_ANSWERS,
+            variables: { useditemQuestionId: props.Comments?._id },
+          },
+        ],
+      });
+      props.IsOpen(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  // 대댓글 수정 함수
+  const onClickEditAnswer = async () => {
+    if (contents === "") { 
+      alert("수정된 내용이 없습니다")
+      return
+    }
+    try { 
+      const result = await updateUseditemQuestionAnswer({
+        variables: {
+          updateUseditemQuestionAnswerInput: { contents },
+          useditemQuestionAnswerId: props.el._id,
+        },
+        refetchQueries: [
+          {
+            query: FETCH_USEDITEM_QUESTION_ANSWERS,
+            variables: { useditemQuestionId: props.Comments?._id },
+          },
+        ],
+      }); props.setIsEdit(false)
+    } catch (error) {console.log(error.message) }
+    
+  };
+  return (
+    <div>
+      <CommnetsWriteInput
+        autoFocus
+        onChange={onChangeAnswer}
+        defaultValue={props.isEdit ? props.el?.contents : ""}
+      />
+      <div style={{ display: "flex" }}>
+        <CommnetsInput
+          readOnly
+          placeholder={contents.length + "/100"}
+        />
+        {props.isEdit ? (
+          <CommentsBnt
+            style={{ backgroundColor: "#B69ACB" }}
+            onClick={onClickIsEdit}
+          >
+            취소
+          </CommentsBnt>
+        ) : (
+          <CommentsBnt
+            style={{ backgroundColor: "#B69ACB" }}
+            onClick={onClickIsOpen}
+          >
+            취소
+          </CommentsBnt>
+        )}
+
+        {props.isEdit ? (
+          <CommentsBnt onClick={onClickEditAnswer}>수정</CommentsBnt>
+        ) : (
+          <CommentsBnt onClick={onClickSubmitAnswer}>등록</CommentsBnt>
+        )}
+      </div>
+    </div>
+  );
+}
