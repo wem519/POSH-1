26bee158c72af5cc80050f121bd69f7748a48294
user <filament3@gmail.com>
2021-11-16 13:33:31 +0900

    Update : 상품이름, 상세주소 유효성검사 추가

--- src/components/units/products/write/ProductWrite.validation.ts
+++ src/components/units/products/write/ProductWrite.validation.ts
@@ -1,11 +1,15 @@
 import * as yup from "yup";
 
 export const schema = yup.object({
-  name: yup.string().required("제목을 입력해주세요!"),
+  name: yup
+    .string()
+    .max(30, "30자 이내로 입력해주세요!")
+    .required("제목을 입력해주세요!"),
   price: yup
     .number()
     .required("가격을 입력해주세요!")
     .min(0)
     .typeError("숫자만 입력해주세요!"),
   contents: yup.string().required("상세내용을 입력해주세요!"),
+  addressDetail: yup.string().required("상세주소를 입력해주세요!"),
 });
