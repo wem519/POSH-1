c17f859f7995bbcf33884f8eb427e8eeedf2e023
user <filament3@gmail.com>
2021-11-17 11:45:24 +0900

    Fix : WithAuth - fetchUserLoggedIn으로 로그인되지 않은 유저 거르기

--- src/components/commons/hocs/WithAuth.tsx
+++ src/components/commons/hocs/WithAuth.tsx
@@ -1,16 +1,19 @@
+import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import { useEffect, useContext } from "react";
 import { GlobalContext } from "../../../../pages/_app";
+import { FETCH_USER_LOGGED_IN } from "../../units/accounts/login /Login.queries";
 
 export const WithAuth = (Component) => (props) => {
   const router = useRouter();
   const { accessToken } = useContext(GlobalContext);
+  const { data } = useQuery(FETCH_USER_LOGGED_IN);
 
   useEffect(() => {
-    if (!localStorage.getItem("refreshToken")) {
+    if (!localStorage.getItem("refreshToken") || !data) {
       alert("Posh에 입장해 주세요");
       router.push("/posh/accounts/login");
     }
-  }, [accessToken]);
+  }, [accessToken, data]);
   return <Component {...props} />;
 };
