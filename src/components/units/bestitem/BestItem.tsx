c175abee3fead9230316ccabf1bd4863361b4b79
wem519 <wem519@gmail.com>
2021-11-21 17:03:00 +0900

    Add : 홈 화면 상단 캐러셀 추가

--- src/components/units/bestitem/BestItem.tsx
+++ src/components/units/bestitem/BestItem.tsx
@@ -1,50 +1,60 @@
 import { gql, useQuery } from "@apollo/client";
 import styled from "@emotion/styled";
-import { useRouter } from "next/router";
 import { v4 as uuidv4 } from "uuid";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Wrapper = styled.div`
   display: flex;
-  align-items: center;
-  margin-top: 13px;
-  @media screen and (min-width: 600px) {
-    display: none;
-  }
+  margin-top: 40px;
+  margin-bottom: 100px;
 `;
 const PictureBox = styled.div`
-  width: 70px;
-  height: 70px;
-  margin: 0px 10px;
+  width: 1050px;
+  height: 300px;
+  /* padding: 10px; */
+  margin: 0 auto;
   border: 3px solid transparent;
-  border-radius: 50px;
-  background-image: linear-gradient(#fff, #fff),
-    linear-gradient(to right, #ffefe1bd, #ffcdf3aa, #5d1d8caa);
-  background-origin: border-box;
-  background-clip: content-box, border-box;
   background-color: #fff;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   cursor: pointer;
+  /* padding: 10px; */
 `;
-const Picture = styled.img`
-  object-fit: cover;
-  width: 90%;
-  height: 90%;
-  border-radius: 50%;
-  /* background-color: gray; */
-`;
-const ProfileWrpper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+const SliderBest = styled(Slider)`
+  .slick-track .slick-center {
+    -webkit-transform: scale(1.5, 1.75);
+    -moz-transform: scale(1.5, 1.75);
+    transform: scale(1.5, 1.7);
+  }
+  .slick-track {
+    display: flex;
+  }
+  .slick-track .slick-slide {
+    /* display: flex; */
+    width: 300px;
+    height: 300px;
+    padding: 20px;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
+const Picture = styled.img`
+  width: 300px;
+  height: 300px;
+  object-fit: cover;
+`;
+const ProfileWrpper = styled.div`
+  /* display: flex;
+  flex-direction: column;
+  align-items: center; */
+`;
 const FETCH_USEDITEMS_OF_BEST = gql`
   query fetchUseditemsOfTheBest {
     fetchUseditemsOfTheBest {
       _id
       pickedCount
+      images
       seller {
         picture
         name
@@ -55,23 +65,44 @@ const FETCH_USEDITEMS_OF_BEST = gql`
 `;
 
 export default function BestItem() {
-  const router = useRouter();
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 3000,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "20px",
+    focusOnSelect: true,
+    // fade: true,
+    arrows: true, // 이거 true(default)하면 화살표영역때문에 왼쪽으로 밀림
+  };
 
   const { data } = useQuery(FETCH_USEDITEMS_OF_BEST);
 
-  const onClickMoveSeller = (e: any) =>
-    router.push(`/posh/products/${e.currentTarget.id}/seller`);
-
   return (
     <Wrapper>
-      {data?.fetchUseditemsOfTheBest.map((el: any) => (
-        <ProfileWrpper key={uuidv4()}>
-          <PictureBox onClick={onClickMoveSeller} id={el._id}>
-            <Picture src={el.seller.picture} />
-          </PictureBox>
-          <div style={{ fontSize: "small" }}>{el.seller.name}</div>
-        </ProfileWrpper>
-      ))}
+      <ProfileWrpper>
+        <PictureBox>
+          <SliderBest {...settings}>
+            {data?.fetchUseditemsOfTheBest.map((el: any) => (
+              <Picture
+                key={uuidv4()}
+                src={
+                  el.images.filter((el: any) => el)[0]
+                    ? `https://storage.googleapis.com/${
+                        el.images.filter((el: any) => el)[0]
+                      }`
+                    : "/images/noImage.png"
+                }
+              />
+            ))}
+          </SliderBest>
+        </PictureBox>
+      </ProfileWrpper>
     </Wrapper>
   );
 }
